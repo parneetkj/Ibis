bfb026cbbcfd27f57c0b75fb1e2a4992d6e13124
JanetThomas123 <115407383+JanetThomas123@users.noreply.github.com>
2022-11-13 17:18:57 +0000

    Added SignUpForm class
    -adds length validation to HTML file

--- lessons/forms.py
+++ lessons/forms.py
@@ -6,3 +6,12 @@ class RequestForm(forms.ModelForm):
     class Meta:
         model = Request
         exclude = ['status']
+    
+    
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(label='First name', max_length=50)
+    last_name = forms.CharField(label='Last name', max_length=50)
+    email = forms.CharField(label='Email', max_length=50)
+    new_password = forms.CharField(label='password', widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+    
