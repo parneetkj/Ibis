25fe21b74958a22a4804f43e2776715c8b4e5c3a
Parneet Johal <parneet.johal@kcl.ac.uk>
2022-11-14 20:48:57 +0000

    Add Booking model

--- lessons/models.py
+++ lessons/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+from django.db.models import TimeField, DateTimeField
 
 # Create your models here.
 class Request(models.Model):
@@ -56,4 +57,45 @@ class Request(models.Model):
         max_length=20,
         default = "In Progress"
     )
-    
+
+class Booking(models.Model):
+    """ Booking by admin """
+
+    student = models.CharField(max_length=50, blank=False)
+
+    day = models.CharField(max_length=10, blank=False)
+
+    time = models.TimeField()
+
+    start_date = models.DateTimeField()
+
+    DURATION_CHOICES = [
+    (15, '15 Minuite'),
+    (30, '30 Minuite'),
+    (45, '45 Minuite'),
+    (60, '1 Hour'),
+    ]
+    duration = models.IntegerField(
+    choices=DURATION_CHOICES,
+    default=30
+    )
+
+    interval = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=1,
+                message="Must book at least 1 week of lessons."
+            ),
+            MaxValueValidator(
+                limit_value=4,
+                message="Interval cannot be more than a month."
+            )
+        ]
+    )
+
+    teacher = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    no_of_lessons = models.IntegerField(default=1)
