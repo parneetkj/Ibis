c5e194f49bc86bd21cbc70d449f63fe14f2275a4
Mathew Tran <m-mathew@hotmail.co.uk>
2022-11-14 10:16:33 +0000

    Add home_page

--- Ibis/urls.py
+++ Ibis/urls.py
@@ -1,4 +1,4 @@
-"""msms URL Configuration
+"""Ibis URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lessons import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home_page, name ='home_page'),
 ]
--- Ibis/wsgi.py
+++ Ibis/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for msms project.
+WSGI config for Ibis project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ibis.settings')
 
 application = get_wsgi_application()
