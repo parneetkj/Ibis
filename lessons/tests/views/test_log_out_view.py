d5d9b401c5cea7f0c5410fc67ad904490f0b9dd5
Janet <k21003182@kcl.ac.uk>
2022-11-26 22:32:19 +0000

    tests for log-out

--- lessons/tests/views/test_log_out_view.py
+++ lessons/tests/views/test_log_out_view.py
@@ -4,27 +4,26 @@ from django.urls import reverse
 from lessons.models import User
 from lessons.helpers import LogInTester
 
-# class LogOutViewTestCase(TestCase, LogInTester):
-#     """Tests of the log out view."""
+class LogOutViewTestCase(TestCase, LogInTester):
+    """Tests of the log out view."""
 
-#     def setUp(self):
-#         self.url = reverse('log_out')
-#         self.user = User.objects.create_user('johndoe@example.org',
-#             first_name='John',
-#             last_name='Doe',
-#             #email='johndoe@example.org',
-#             password='Password123',
-#             is_active=True,
-#         )
+    def setUp(self):
+        self.url = reverse('log_out')
+        self.user = User.objects.create_user('johndoe@example.org',
+            first_name='John',
+            last_name='Doe',
+            password='Password123',
+            is_active=True,
+        )
 
-#     def test_log_out_url(self):
-#         self.assertEqual(self.url,'/log_out/')
+    def test_log_out_url(self):
+        self.assertEqual(self.url,'/log_out/')
 
-#     def test_get_log_out(self):
-#         self.client.login(username='johndoe@example.org', password='Password123')
-#         self.assertTrue(self._is_logged_in())
-#         response = self.client.get(self.url, follow=True)
-#         response_url = reverse('home')
-#         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
-#         self.assertTemplateUsed(response, 'home.html')
-#         self.assertFalse(self._is_logged_in())
+    def test_get_log_out(self):
+        self.client.login(username='johndoe@example.org', password='Password123')
+        self.assertTrue(self._is_logged_in())
+        response = self.client.get(self.url, follow=True)
+        response_url = reverse('home_page')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'home_page.html')
+        self.assertFalse(self._is_logged_in())
