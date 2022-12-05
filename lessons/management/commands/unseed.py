3c51e9fb43cf6c2573a532aa63cadfb30dd280a4
Parneet Johal <parneet.johal@kcl.ac.uk>
2022-12-05 14:44:22 +0000

    Deseed deletes directors as well

--- lessons/management/commands/unseed.py
+++ lessons/management/commands/unseed.py
@@ -3,6 +3,6 @@ from lessons.models import User, Request, Booking
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User.objects.filter(is_director=False).delete()
+        User.objects.filter().delete()
         Request.objects.filter().delete()
         Booking.objects.filter().delete()
