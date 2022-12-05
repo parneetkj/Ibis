50c1171adf5a115da477b871622ed1d44fd39844
William Lingard <William.Lingard@kcl.ac.uk>
2022-12-05 22:33:16 +0000

    Add tests for transfers.

--- lessons/tests/forms/test_transfer_form.py
+++ lessons/tests/forms/test_transfer_form.py
@@ -1,25 +1,34 @@
-from django import forms
+from lessons.models import User
 from django.test import TestCase
 from lessons.forms import TransferForm
+from django import forms
 
 class TransferFormTestCase(TestCase):
-    def setUp(self):
-        self.form_input = {'amount_paid': 8.12}
+    fixtures = ['lessons/tests/fixtures/default_user.json']
 
+    def setUp(self):
+        self.user = User.objects.get(username='johndoe@example.org')
+        self.form_input = {'student': self.user, 'amount': 8.12}
+        
     def test_form_contains_required_fields(self):
         form = TransferForm()
-        self.assertIn('amount_paid', form.fields)
+        self.assertIn('amount', form.fields)
+        amount = form.fields['amount']
+        self.assertTrue(isinstance(amount, forms.DecimalField))
+        self.assertIn('student', form.fields)
+        student = form.fields['student']
+        self.assertTrue(isinstance(student, forms.ModelChoiceField))
 
     def test_form_accepts_valid_input(self):
         form = TransferForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_form_rejects_blank_input(self):
-        self.form_input['amount_paid'] = ''
+        self.form_input['amount'] = ''
         form = TransferForm(data=self.form_input)
         self.assertFalse(form.is_valid())
     
     def test_form_rejects_invalid_number(self):
-        self.form_input['amount_paid'] = 9.999
+        self.form_input['amount'] = 9.999
         form = TransferForm(data=self.form_input)
         self.assertFalse(form.is_valid())
