0209415279b2ef2c841707a1fa70ed13d734f000
Parneet Johal <parneet.johal@kcl.ac.uk>
2022-12-08 02:28:33 +0000

    Add methods to seed requests, bookings and tranfers to the database

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,36 +1,53 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from lessons.models import User
+from lessons.models import User, Request, Booking, Transfer, Invoice
+from random import randint, random
+from decimal import Decimal
+from lessons.helpers import create_transfer, calculate_student_balance
+
+#import pytz
 
 class Command(BaseCommand):
     PASSWORD = "Password123"
     USER_COUNT = 100
+    REQUEST_COUNT = 50
+    BOOKING_COUNT = 100
+    PAID_PROBABILITY = 0.75
+    OVER_PAID_PROBABILITY = 0.1
+    PARTIAL_PAID_PROBABILITY = 0.25
+
 
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
 
     def handle(self, *args, **options):
-        self._create_users()
-        self._create_admin("Petra", "Pickles")
-        self._create_director("Marty", "Major")
+        self.create_users()
+        self.create_admin("Petra", "Pickles")
+        self.create_admin("David", "Dawson")
+        self.create_director("Marty", "Major")
 
-    def _create_users(self):
-        self._create_user("John", "Doe")
+        self.students = User.objects.all().filter(is_student=True)
+
+        self.create_requests()
+        self.create_bookings()
+
+    def create_users(self):
+        self.create_user("John", "Doe")
         user_count = 1
         while user_count < self.USER_COUNT:
             print(f"Seeding user {user_count}/{self.USER_COUNT}", end='\r')
             try:
                 first_name = self.faker.first_name()
                 last_name = self.faker.last_name()
-                self._create_user(first_name, last_name)
+                self.create_user(first_name, last_name)
             except:
                 continue
             user_count += 1
         print("User seeding complete.      ")
 
-    def _create_user(self, first_name, last_name ):
-        username = self._email(first_name, last_name)
+    def create_user(self, first_name, last_name ):
+        username = self.email(first_name, last_name)
         User.objects.create_user(
             username=username,
             first_name=first_name,
@@ -39,8 +56,8 @@ class Command(BaseCommand):
             is_student=True
         )
 
-    def _create_admin(self, first_name, last_name ):
-        username = self._email(first_name, last_name)
+    def create_admin(self, first_name, last_name ):
+        username = self.email(first_name, last_name)
         User.objects.create_user(
             username=username,
             first_name=first_name,
@@ -49,8 +66,8 @@ class Command(BaseCommand):
             is_admin=True
         )
     
-    def _create_director(self, first_name, last_name ):
-        username = self._email(first_name, last_name)
+    def create_director(self, first_name, last_name ):
+        username = self.email(first_name, last_name)
         User.objects.create_user(
             username=username,
             first_name=first_name,
@@ -60,6 +77,78 @@ class Command(BaseCommand):
             is_director=True
         )
 
-    def _email(self, first_name, last_name):
+    def create_requests(self):
+        for i in range(self.REQUEST_COUNT):
+            print(f"Seeding requests {i}/{self.REQUEST_COUNT}", end='\r')
+            self.create_request()
+        print("Request seeding complete.      ")
+
+    def create_request(self):
+        request = Request(
+            student=self.get_random_student(),
+            date="2023-12-12",
+            time="10:51",
+            amount=randint(1,50),
+            interval=randint(1,4),
+            duration=60,
+            topic = "Topic",
+            teacher = "Ms.Test",
+        )
+        request.save()
+
+    def create_bookings(self):
+        for i in range(self.BOOKING_COUNT):
+            print(f"Seeding booking {i}/{self.BOOKING_COUNT}", end='\r')
+            self.create_booking()
+        print("Booking seeding complete.      ")
+
+    def create_booking(self):
+        booking = Booking(
+            student=self.get_random_student(),
+            day="Monday",
+            start_date="2023-12-12",
+            time="10:51",
+            no_of_lessons=randint(1,50),
+            interval=randint(1,4),
+            duration=60,
+            topic = "Topic",
+            teacher = "Ms.Test",
+            cost=randint(1,20)
+        )
+        booking.save()
+        booking.generate_invoice()
+        booking_invoice = Invoice.objects.get(booking=booking)
+        if random() < self.PAID_PROBABILITY:
+            self.pay_invoice(booking_invoice)
+        
+        calculate_student_balance(booking.student)
+
+    def pay_bookings(self):
+        for i in range(self.BOOKING_COUNT):
+            print(f"Adding Payment {i}/{self.BOOKING_COUNT}", end='\r')
+            self.pay_booking()
+        print("Payment seeding complete.      ")
+
+    def pay_invoice(self, invoice):
+        random_num = random()
+        if random_num < self.OVER_PAID_PROBABILITY:
+            pay_amount = invoice.total_price * Decimal(1.2)
+        elif random_num < self.PARTIAL_PAID_PROBABILITY:
+            pay_amount = invoice.total_price * Decimal(0.5)
+        else:
+            pay_amount = invoice.total_price
+        
+        #invoice.add_partial_payment(pay_amount)
+        create_transfer(invoice, pay_amount)
+        invoice.check_if_paid()
+        invoice.change_invoice_amount()
+
+        
+
+    def get_random_student(self):
+        index = randint(0,self.students.count()-1)
+        return self.students[index]
+
+    def email(self, first_name, last_name):
         email = f'{first_name.lower()}.{last_name.lower()}@example.org'
         return email
--- lessons/management/commands/unseed.py
+++ lessons/management/commands/unseed.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
-from lessons.models import User, Request, Booking
+from lessons.models import User, Request, Booking, Invoice, Transfer
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.filter().delete()
         Request.objects.filter().delete()
         Booking.objects.filter().delete()
+        Invoice.objects.filter().delete()
+        Transfer.objects.filter().delete()
